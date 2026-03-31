941a44fe531a85e66ba06700d31365fc2096c596
anshupriya380 <145607444+anshupriya380@users.noreply.github.com>
2026-03-31 22:01:48 +0530

    31 March

---
+++ Loops.js
@@ -0,0 +1,41 @@
+// print 5 times "apna javascript"
+for(let i=1;i<=5;i++){
+    console.log("apna javascript");
+} 
+
+// print sum of first 5 natural numbers
+{
+    let sum=0;
+    for(let i=1;i<=5;i++){
+        sum+=i;
+    }
+    console.log(sum);
+
+}
+
+// print sum of first n natural numbers
+// let n= prompt("Enter a number");
+// {
+//     let sum=0;
+//     for(let i=0;i<=n;i++){
+//         sum = sum + i;
+//     }
+//     console.log(sum);
+// }
+
+//print using while loop
+let i=0;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+// print using do while loop
+{
+let i = 0;
+do{
+console.log("Anshu Priya" , i);
+i++;
+}while (i<=5);
+}
+
